c5427c7bf16b993a7db07ca0a358bd4357d165c9
franmacke <fmacke10@gmail.com>
2024-11-11 16:33:04 -0300

    Enhance AboutMe and Projects components with internationalization; update translations for project details and personal description

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,15 +1,29 @@
+import { useTranslation } from "react-i18next";
+import Card from "./Card";
 import Section from "./Section";
 
 
 
 const AboutMe = () => {
+
+    const { t } = useTranslation();
+
     return (
-        <Section title="About Me">
-            <div className="p-10">
-                <p>
-                    I am a developer with a passion for all kinds of software. I have experience with a variety of technologies. I am always looking to learn new things and improve my skills.
-                </p>
-            </div>
+        <Section title={t("aboutMe.title")}>
+            <Card>
+                <div className="flex flex-col md:flex-row items-center">
+                    <img 
+                        className="w-40 h-40 rounded-full" 
+                        src="https://media.licdn.com/dms/image/v2/D4D03AQE4DoeyigO_LQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1730849767922?e=1736985600&v=beta&t=eaonM2JPc7bN2CgGfet4vw7tLZj8abVuINF6j977dgQ" 
+                        alt="Franco Macke" 
+                    />
+                    <div className="md:ml-5 mt-5 md:mt-0 text-left">
+                        <h2 className="text-2xl font-bold">Franco Macke</h2>
+                        <p className="text-gray-500">{t("aboutMe.career")}</p>
+                        <p className="mt-5">{t("aboutMe.description")}</p>
+                    </div>
+                </div>
+            </Card>
         </Section>
     )
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,49 +1,53 @@
+import { useTranslation } from "react-i18next";
 import { ProjectProps } from "../props/ProjectProps";
 import ProjectInfo from "./ProjectInfo";
 import Section from "./Section";
 
 
-const projectList: ProjectProps[] = [
-    {
-        title: "Discord Bot",
-        description: "Developed a Discord bot that can be used to manage a server, play music, and more.",
-        link: "#",
-        technologies: ["Python", "MySQL", "Discord API"],
-        image: "discord_bot.png"
-    },
-    {
-        title: "Pong Game",
-        description: "Developed a Pong game using Python and Pygame, with local multiplayer support.",
-        link: "https://github.com/franmacke/pong",
-        technologies: ["Python", "Pygame", "OOP", "Game Development"],
-        image: "pong.png"
-    },
-    {
-        title: "Football Team Balancer [WIP]",
-        description: "Developed a web application that can be used to balance football teams.",
-        link: "https://colab.research.google.com/drive/1zUPE1jlksdKGAVveFxUxwhS-JPlAmfNM?usp=sharing",
-        technologies: ["Python", "Pandas", "Colab", "Data Analysis"],
-        image: "discord_bot.png"
-    },
-    {
-        title: "Portfolio",
-        description: "Developed my personal portfolio using React and Tailwind CSS.",
-        link: "#",
-        technologies: ["React", "Tailwind CSS", "TypeScript"],
-        image: ""
-    },
-    {
-        title: "Drink Maker",
-        description: "Developed with a friend a drink maker website using ingredients from a database.",
-        link: "https://relaxed-custard-a3eca2.netlify.app/",
-        technologies: ["HTML", "CSS", "JavaScript", "APIs"],
-        image: "drink_maker.png"
-    }
-]
-
 const Projects = () => {
+
+    const { t } = useTranslation();
+
+    const projectList: ProjectProps[] = [
+        {
+            title: t("projects.items.discordBot.name"),
+            description: t("projects.items.discordBot.description"),
+            link: "#",
+            technologies: ["Python", "MySQL", "Discord API"],
+            image: "discord_bot.png"
+        },
+        {
+            title: t("projects.items.pongGame.name"),
+            description: t("projects.items.pongGame.description"),
+            link: "https://github.com/franmacke/pong",
+            technologies: ["Python", "Pygame", "OOP", "Game Development"],
+            image: "pong.png"
+        },
+        {
+            title: t("projects.items.footballTeamBalancer.name"),
+            description: t("projects.items.footballTeamBalancer.description"),
+            link: "https://colab.research.google.com/drive/1zUPE1jlksdKGAVveFxUxwhS-JPlAmfNM?usp=sharing",
+            technologies: ["Python", "Pandas", "Colab", "Data Analysis"],
+            image: "discord_bot.png"
+        },
+        {
+            title: t("projects.items.portfolio.name"),
+            description: t("projects.items.portfolio.description"),
+            link: "https://github.com/franmacke/portfolio/tree/dev",
+            technologies: ["React", "Tailwind CSS", "TypeScript"],
+            image: ""
+        },
+        {
+            title: t("projects.items.drinkMaker.name"),
+            description: t("projects.items.drinkMaker.description"),
+            link: "https://relaxed-custard-a3eca2.netlify.app/",
+            technologies: ["HTML", "CSS", "JavaScript", "APIs"],
+            image: "drink_maker.png"
+        }
+    ]
+    
     return (
-        <Section title="Projects">
+        <Section title={t("projects.title")}>
             <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-5">
                 { projectList.map((project, index) => (
                     <ProjectInfo key={index} project={project} />
