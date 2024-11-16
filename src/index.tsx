2edb3b40ab11cba20a7ad69f202f6910d61d206e
franmacke <fmacke10@gmail.com>
2024-11-16 11:51:33 -0300

    Vercel Speed Insights

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
     <Analytics />
+    <SpeedInsights />
+    <App />
   </React.StrictMode>
 );
 
